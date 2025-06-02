57b60ba2be06399e8952cda35ba86e73eb113211
BrianDelalex <brian.delalex@outlook.com>
2025-06-02 09:51:04 +0200

    Adding multiboot_header.asm.

---
+++ src/arch/x86_64/boot/multiboot_header.asm
@@ -0,0 +1,24 @@
+;*******************************************************************
+;**
+;**  This file is part of SNUTOs project, and is made available under
+;**  the terms of the GNU General Public License version 3.
+;**
+;**  Copyright (C) 2025 - Brian DELALEX-FONDU
+;**
+;*******************************************************************
+
+section .multiboot
+header_start:
+    dd 0xe85250d6                ; magic number (multiboot 2)
+    dd 0                         ; architecture 0 (protected mode i386)
+    dd header_end - header_start ; header length
+    ; checksum
+    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+
+    ; insert optional multiboot tags here
+
+    ; required end tag
+    dw 0    ; type
+    dw 0    ; flags
+    dd 8    ; size
+header_end:
