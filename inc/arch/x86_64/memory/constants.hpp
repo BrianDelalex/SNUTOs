a371c2e1a222b7dfaf53cada2c347e2723c54f14
BrianDelalex <brian.delalex@outlook.com>
2025-10-07 18:11:35 +0200

    Adding header file to define usefull memory related constants.

---
+++ inc/arch/x86_64/memory/constants.hpp
@@ -0,0 +1,21 @@
+/*******************************************************************\
+**
+**  This file is part of SNUTOs project, and is made available under
+**  the terms of the GNU General Public License version 3.
+**
+**  Copyright (C) 2025 - Brian DELALEX-FONDU
+**
+\*******************************************************************/
+
+#ifndef MEM_CONSTANTS_HPP
+#define MEM_CONSTANTS_HPP
+
+# define NULL ((void*)0)
+
+# define PAGE_SIZE 0x1000
+
+extern char KERNEL_VIRT_END[];
+
+extern char KERNEL_VIRT_START[];
+
+#endif//!MEM_CONSTANTS_HPP
