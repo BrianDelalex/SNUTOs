9f49ca7e579634db8f32b35a74b0ec847a2a3e35
BrianDelalex <brian.delalex@outlook.com>
2025-10-13 15:29:10 +0200

    Modifying kmain to initialize and test latest features.

--- src/kmain.cpp
+++ src/kmain.cpp
@@ -11,17 +11,49 @@
 # include <drivers/multiboot/multiboot_info.hpp>
 # include <arch/x86_64/memory/page_frame_allocator.hpp>
 # include <drivers/serial/SerialStream.hpp>
+# include <arch/x86_64/memory/virtual_memory.hpp>
+# include <arch/x86_64/memory/paging.h>
+# include <arch/x86_64/memory/kheap.hpp>
+# include <kernel/memory/alloc.hpp>
 
+
+extern KHeap g_kheap;
 multiboot_info *mb_info = nullptr;
 
 void show_memory_map_infos();
 void parse_multiboot_infos_tags();
 void show_memory_multiboot_tags();
+void init_virtual_memory_manager();
 extern VGA vga;
 
-extern char KERNEL_VIRT_END[];
-extern char KERNEL_VIRT_START[];
+extern char PD[];
+extern char PT[];
 
+void count_mapped_page()
+{
+    int page_allocated = 0;
+    struct pt_s* pt = (struct pt_s*)PT;
+    uint64_t first_page_mapped = 0xFFFFFFFFFFFFFFFF;
+    uint64_t last_page_mapped;
+
+    for (int i = 0; i < BOOTSTRAP_PAGE_TABLE_NBR ; i++) {
+        for (int j = 0; j < 512; j++) {
+            if (pt[i].entries[j].addr != 0) {
+                if (pt[i].entries[j].bits.p) {
+                    if (pt[i].entries[j].addr < first_page_mapped)
+                        first_page_mapped = pt[i].entries[j].addr;
+                    page_allocated++;
+                    last_page_mapped = pt[i].entries[j].addr;
+                }
+            }
+        }
+    }
+    vga << "Page mapped " << page_allocated << " from " << HEX << \
+        first_page_mapped << " to " << HEX << last_page_mapped << " size " << \
+        (last_page_mapped - first_page_mapped) / 1024 / 1024 << " MB." << "\n";
+}
+
+bool is_address_mapped(void *addr);
 extern "C" void kmain(__attribute__((unused)) uint32_t mb_magic, uint32_t mb_addr)
 {
     SerialStream serial;
@@ -35,7 +67,24 @@ extern "C" void kmain(__attribute__((unused)) uint32_t mb_magic, uint32_t mb_add
 
     vga << "Kernel virtual end: " << HEX << ((uint64_t)KERNEL_VIRT_END) << "\n";
 
-    vga << "Kernel size: " << HEX << ((uint64_t)KERNEL_VIRT_END) - ((uint64_t)KERNEL_VIRT_START) << "\n";
+    vga << "Kernel size: " << ((uint64_t)KERNEL_VIRT_END) - ((uint64_t)KERNEL_VIRT_START) << "\n";
+
+    init_kernel_frame_map();
+
+    count_mapped_page();
+    init_virtual_memory_manager();
+    count_mapped_page();
+
+
+    g_kheap.ShowBlocks(serial);
+    char *ptr = (char*) kmalloc(11);
+    g_kheap.ShowBlocks(serial);
+    char *ptr2 = (char*)kmalloc(512);
+    g_kheap.ShowBlocks(serial);
+    kfree(ptr);
+    g_kheap.ShowBlocks(serial);
+    kfree(ptr2);
+    g_kheap.ShowBlocks(serial);
 
     while (1) {
         asm volatile("hlt");
